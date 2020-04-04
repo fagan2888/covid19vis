2ac3b7f87804805ad32a1d47d294bc3fdc937032
vgrem <vvgrem@gmail.com>
2020-04-04 23:06:14 +0300

    Updates

---
+++ precache-manifest.ad9a8758b8ebcfdb77091bd9fc8e40ee.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e89f5c5fd67aa1a50e8e2cb99654147",
+    "url": "/covid19vis/index.html"
+  },
+  {
+    "revision": "72b56dab1a7b0c0ed494",
+    "url": "/covid19vis/static/css/main.f6ea4e68.chunk.css"
+  },
+  {
+    "revision": "f6738248af7709715002",
+    "url": "/covid19vis/static/js/2.1142f222.chunk.js"
+  },
+  {
+    "revision": "278be41389e3536a5347a9018737729d",
+    "url": "/covid19vis/static/js/2.1142f222.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "72b56dab1a7b0c0ed494",
+    "url": "/covid19vis/static/js/main.614b04db.chunk.js"
+  },
+  {
+    "revision": "56c1d4d0e8bdab94b9fe",
+    "url": "/covid19vis/static/js/runtime-main.0e6bf9a6.js"
+  }
+]);
